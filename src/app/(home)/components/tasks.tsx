884ff88201fa27e3cd09b157ff0e161287ce80ef
davitti <abuadonald@gmail.com>
2024-12-29 22:36:38 +0100

    Add Task component rendering to Tasks component.

--- src/app/(home)/components/tasks.tsx
+++ src/app/(home)/components/tasks.tsx
@@ -1,6 +1,13 @@
 import Flex from "@/components/layouts/flex";
 import React from "react";
+import Task from "./task";
 
 export default function Tasks() {
-	return <></>;
+	return (
+		<Flex flex='column' className='gap-3 basis-[736px] h-full'>
+			{[2, 3].map((task) => (
+				<Task key={task} />
+			))}
+		</Flex>
+	);
 }
