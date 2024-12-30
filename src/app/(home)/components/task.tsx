7eb270c95be219b62e1eb6a782e11c07dc3e8863
davitti <abuadonald@gmail.com>
2024-12-30 22:01:09 +0100

    Refactor Task component to accept props and display task title.

--- src/app/(home)/components/task.tsx
+++ src/app/(home)/components/task.tsx
@@ -5,23 +5,25 @@ import { useSetAtom } from "jotai";
 import { Check, Trash2 } from "lucide-react";
 import React from "react";
 
-export default function Task() {
+interface Task {
+	task: any;
+}
+export default function Task({ task }: Task) {
 	const task_form_setter = useSetAtom(task_form_jotai);
 
 	return (
-		<Flex className='gap-3 bg-[#262626] w-full text-white items-center cursor-pointer rounded-lg '>
+		<Flex className='shrink-0 gap-3 bg-[#262626] w-full text-white items-center cursor-pointer rounded-lg '>
 			{/* Checkbox */}
 			<div className='cursor-pointer min-w-5 min-h-5 p-1 outline outline-2 outline-[#4EA8DE] rounded-full overflow-clip'>
 				{true && <Check size={12} />}
 			</div>
 			<p
-				className='active:scale-[.99] transition'
+				className='active:scale-[.99] transition w-full'
 				onClick={() => task_form_setter("edit-task")}
 			>
-				Integer urna interdum massa libero auctor neque turpis turpis semper.
-				Duis vel sed fames integer.
+				{task.title}
 			</p>
-			<InteractiveIcon>
+			<InteractiveIcon className='ml-auto'>
 				<Trash2 className='text-[#808080]' />
 			</InteractiveIcon>
 		</Flex>
