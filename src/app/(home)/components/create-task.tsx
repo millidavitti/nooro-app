23eb8b6138ba6f831ce27fa7071d9d114eaa1337
davitti <abuadonald@gmail.com>
2024-12-30 02:18:36 +0100

    Add state management for task form and update button functionality in CreateTask component.

--- src/app/(home)/components/create-task.tsx
+++ src/app/(home)/components/create-task.tsx
@@ -1,12 +1,19 @@
 import Flex from "@/components/layouts/flex";
 import Button from "@/components/ui/button";
+import { task_form_jotai } from "@/data/atoms/ui_state";
+import { useSetAtom } from "jotai";
 import { CirclePlus } from "lucide-react";
 import React from "react";
 
 export default function CreateTask() {
+	const task_form_setter = useSetAtom(task_form_jotai);
+
 	return (
 		<Flex flex='row' className='p-0 w-full justify-center shrink-0'>
-			<Button className='flex w-full basis-[736px] p-4 gap-3 justify-center bg-[#1E6F9F] text-white'>
+			<Button
+				onClick={() => task_form_setter("create-task")}
+				className='flex w-full basis-[736px] p-4 gap-3 justify-center bg-[#1E6F9F] text-white'
+			>
 				Create Task
 				<CirclePlus />
 			</Button>
