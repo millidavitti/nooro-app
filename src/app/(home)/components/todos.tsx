fb7d8a50eed73374edac90ddcc063ce650a09054
davitti <abuadonald@gmail.com>
2024-12-30 02:19:09 +0100

    Implement conditional rendering for task creation/editing in Todos component.

--- src/app/(home)/components/todos.tsx
+++ src/app/(home)/components/todos.tsx
@@ -1,15 +1,28 @@
+"use client";
 import Flex from "@/components/layouts/flex";
 import React from "react";
 import CreateTask from "./create-task";
 import TaskInfo from "./task-info";
 import TaskTray from "./task-tray";
+import { useAtomValue } from "jotai";
+import { task_form_jotai } from "@/data/atoms/ui_state";
+import CreateEditTask from "./create-edit-task";
 
 export default function Todos() {
+	const task_form = useAtomValue(task_form_jotai);
 	return (
-		<Flex className='items-center w-full h-full bg-[#1A1A1A]' flex='column'>
-			<CreateTask />
-			<TaskInfo />
-			<TaskTray />
-		</Flex>
+		<>
+			<Flex className='items-center w-full h-full bg-[#1A1A1A]' flex='column'>
+				{!Boolean(task_form) ? (
+					<>
+						<CreateTask />
+						<TaskInfo />
+						<TaskTray />
+					</>
+				) : (
+					<CreateEditTask />
+				)}
+			</Flex>
+		</>
 	);
 }
