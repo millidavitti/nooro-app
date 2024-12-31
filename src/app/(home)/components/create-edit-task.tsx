85d6757397fd96c43bcc585e392d851add9c659c
davitti <abuadonald@gmail.com>
2024-12-31 01:08:12 +0100

    Refactor task creation logic and integrate custom hook for form handling.

--- src/app/(home)/components/create-edit-task.tsx
+++ src/app/(home)/components/create-edit-task.tsx
@@ -5,10 +5,18 @@ import { useAtom } from "jotai";
 import { ArrowLeft, Check, CirclePlus } from "lucide-react";
 import React from "react";
 import Palette from "./palette";
+import { task_entry_jotai } from "@/data/atoms/app_data";
+import { createTask } from "@/backend/actions/create-task";
+import useCreateEditTaskInterface from "@/hooks/interface/use-create-edit-task-interface";
 
 export default function CreateEditTask() {
-	const [task_form, task_form_setter] = useAtom(task_form_jotai);
-
+	const {
+		createTask,
+		task_entry,
+		task_entry_setter,
+		task_form,
+		task_form_setter,
+	} = useCreateEditTaskInterface();
 	return (
 		<Flex className='w-full justify-center shrink-0'>
 			<Flex
@@ -19,12 +27,12 @@ export default function CreateEditTask() {
 					className='text-white cursor-pointer active:scale-95'
 					onClick={() => task_form_setter(null)}
 				/>
-
+				{/* Create or Edit Task Form */}
 				<form
 					className='flex flex-col gap-3'
 					onSubmit={(e) => {
 						e.preventDefault();
-						task_form_setter(null);
+						createTask();
 					}}
 				>
 					{/* Task Title */}
@@ -33,11 +41,27 @@ export default function CreateEditTask() {
 						<input
 							type='text'
 							required
+							value={task_entry.title}
 							placeholder='Ex. Brush your teeth'
 							className='p-3 outline-none rounded-lg text-white bg-[#262626] placeholder:text-[#333333]'
+							onChange={(e) => {
+								task_entry_setter((task_entry) => {
+									return { ...task_entry, title: e.target.value };
+								});
+							}}
 						/>
 					</Flex>
-					<Palette />
+
+					<Flex flex='column' className='p-0 gap-3 relative'>
+						<input
+							id='color-palette'
+							type='text'
+							required
+							value={task_entry.color}
+							className='absolute bottom-0 opacity- z-[-1] p-3 outline-none rounded-lg text-white bg-[#262626] placeholder:text-[#333333]'
+						/>
+						<Palette />
+					</Flex>
 
 					{/* Task Actions */}
 					<Flex flex='row' className='p-0 w-full justify-center shrink-0'>
