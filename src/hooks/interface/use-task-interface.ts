4543a93b5d4364ea904a7cefde7b4e8696f8a75b
davitti <abuadonald@gmail.com>
2024-12-31 02:34:13 +0100

    Add custom hook for task interface management.

---
+++ src/hooks/interface/use-task-interface.ts
@@ -0,0 +1,14 @@
+import { Task, task_entry_jotai } from "@/data/atoms/app_data";
+import { task_form_jotai } from "@/data/atoms/ui_state";
+import { useSetAtom } from "jotai";
+import React from "react";
+
+export default function useTaskInterface() {
+	const task_form_setter = useSetAtom(task_form_jotai);
+	const task_entry_setter = useSetAtom(task_entry_jotai);
+	function editTask(task: Task) {
+		task_form_setter("edit-task");
+		task_entry_setter(task);
+	}
+	return { editTask };
+}
