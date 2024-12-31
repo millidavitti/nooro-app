e60163cce6b4a071f88c76e6782109af605217af
davitti <abuadonald@gmail.com>
2024-12-31 02:33:55 +0100

    Add editTask function to handle task updates in the backend

---
+++ src/backend/actions/edit-task.ts
@@ -0,0 +1,18 @@
+"use server";
+
+import { TaskEntry } from "@/data/atoms/app_data";
+
+export async function editTask(id: string, update: Partial<TaskEntry>) {
+	const headers = new Headers();
+	headers.append("Content-type", "application/json");
+	try {
+		const res = await fetch(process.env.NOORO_API! + "/tasks/" + id, {
+			method: "PUT",
+			body: JSON.stringify(update),
+			headers,
+		});
+	} catch (error) {
+		console.log("---editTask---\n", error);
+		throw error;
+	}
+}
