f234eb12be3e0299bd09562f1e8178db82d96191
davitti <abuadonald@gmail.com>
2024-12-31 03:57:14 +0100

    Add completed task count to TaskInfo component.

--- src/app/(home)/components/task-info.tsx
+++ src/app/(home)/components/task-info.tsx
@@ -3,7 +3,7 @@ import useTaskInfoInterface from "@/hooks/interface/use-task-info-interface";
 import React from "react";
 
 export default function TaskInfo() {
-	const { taskCount } = useTaskInfoInterface();
+	const { taskCount, completedTaskCount } = useTaskInfoInterface();
 	return (
 		<Flex className='w-full justify-center shrink-0 p-0'>
 			<Flex flex='column' className='p-0 basis-[736px]'>
@@ -12,14 +12,14 @@ export default function TaskInfo() {
 					<Flex flex='row' className='w-fit items-center gap-3 p-0'>
 						<p className='text-[#4EA8DE] font-semibold'>Tasks</p>
 						<div className='p-[2px] px-2 rounded-full text-white text-xs bg-[#333333]'>
-							{taskCount || 0}
+							{taskCount}
 						</div>
 					</Flex>
 					{/* Completed */}
 					<Flex flex='row' className='w-fit items-center gap-3 p-0'>
 						<p className='text-[#8284FA] font-semibold'>Completed</p>
 						<p className='p-[2px] px-2 rounded-full text-white text-xs bg-[#333333]'>
-							0
+							{completedTaskCount} of {taskCount}
 						</p>
 					</Flex>
 				</Flex>
