7050049cff2e03545bfc7c3fa1c843062834f5b5
davitti <abuadonald@gmail.com>
2024-12-31 04:57:34 +0100

    Update CreateTask component layout and styling.

--- src/app/(home)/components/create-task.tsx
+++ src/app/(home)/components/create-task.tsx
@@ -9,7 +9,10 @@ export default function CreateTask() {
 	const task_form_setter = useSetAtom(task_form_jotai);
 
 	return (
-		<Flex flex='row' className='p-0 w-full justify-center shrink-0'>
+		<Flex
+			flex='row'
+			className='p-0 w-full h-14 justify-center shrink-0 absolute top-32'
+		>
 			<Button
 				onClick={() => task_form_setter("create-task")}
 				className='flex w-full basis-[736px] p-4 gap-3 justify-center bg-[#1E6F9F] text-white'
