ad3b5158eb8edd20c3f706b9ed70e8e93b81754b
davitti <abuadonald@gmail.com>
2024-12-31 01:15:51 +0100

    Remove unused React import in useTaskInfoInterface.

--- src/hooks/interface/use-task-info-interface.ts
+++ src/hooks/interface/use-task-info-interface.ts
@@ -1,6 +1,5 @@
 import { tasks_jotai } from "@/data/atoms/app_data";
 import { useAtomValue } from "jotai";
-import React from "react";
 
 export default function useTaskInfoInterface() {
 	const tasks = useAtomValue(tasks_jotai);
