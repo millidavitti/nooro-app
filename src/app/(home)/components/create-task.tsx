e29c61873a64b316c2a32f1564595e77e28c8a9d
davitti <abuadonald@gmail.com>
2024-12-31 04:58:41 +0100

    Remove fixed height from CreateTask component.

--- src/app/(home)/components/create-task.tsx
+++ src/app/(home)/components/create-task.tsx
@@ -11,7 +11,7 @@ export default function CreateTask() {
 	return (
 		<Flex
 			flex='row'
-			className='p-0 w-full h-14 justify-center shrink-0 absolute top-32'
+			className='p-0 w-full justify-center shrink-0 absolute top-32'
 		>
 			<Button
 				onClick={() => task_form_setter("create-task")}
