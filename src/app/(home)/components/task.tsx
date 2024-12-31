0f98c9182c301bc58df2994e8c02909c9e18ddbf
davitti <abuadonald@gmail.com>
2024-12-31 03:28:26 +0100

    Add task completion feature with checkbox and styling updates.

--- src/app/(home)/components/task.tsx
+++ src/app/(home)/components/task.tsx
@@ -9,16 +9,21 @@ interface TaskComponent {
 	task: Task;
 }
 export default function TaskComponent({ task }: TaskComponent) {
-	const { editTask, deleteTask } = useTaskInterface();
+	const { editTask, deleteTask, markTask } = useTaskInterface();
 	return (
 		<Flex className='shrink-0 gap-3 bg-[#262626] w-full text-white items-center cursor-pointer rounded-lg '>
 			{/* Checkbox */}
-			<div className='cursor-pointer min-w-5 min-h-5 p-1 outline outline-2 outline-[#4EA8DE] rounded-full overflow-clip'>
-				{true && <Check size={12} />}
+			<div
+				className='cursor-pointer min-w-5 min-h-5 p-1 data-[is-completed=false]:outline outline-2 data-[is-completed=false]:outline-[#4EA8DE] rounded-full overflow-clip active:scale-95 data-[is-completed=true]:bg-[#5E60CE]'
+				onClick={() => markTask(task.id, { completed: !task.completed })}
+				data-is-completed={task.completed}
+			>
+				{task.completed && <Check size={12} className='stroke-[4px]' />}
 			</div>
 			<p
-				className='active:scale-[.99] transition w-full'
+				className='active:scale-[.99] transition w-full data-[is-completed=true]:line-through data-[is-completed=true]:text-[#808080]'
 				onClick={() => editTask(task)}
+				data-is-completed={task.completed}
 			>
 				{task.title}
 			</p>
